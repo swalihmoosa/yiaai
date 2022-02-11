46785eec55d2937bc8034471c9bdb6487e2a2a9f
swalihmoosa <swalihmoosa72@gmail.com>
2022-02-11 17:52:21 +0530

    Tefun | ongoing

--- src/components/screens/landing/Tefun.jsx
+++ src/components/screens/landing/Tefun.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
+import playImage from '../../assets/images/play-solid.svg'
+
 
 export default function Tefun() {
     return (
@@ -7,23 +9,24 @@ export default function Tefun() {
             <Wrapper>
                 <H3>
                     <B>
-
-                    </B>
+                        Tefun
+                    </B><br />
+                    എന്ന് പറയുമ്പോൾ 
                 </H3>
                 <P>
-
+                ടെക്കിസ്  ക്ലബ്ബിലൂടെ ടെക്നോളജിയിൽ മാറ്റം കൈവരിച്ച ഞങ്ങളുടെ <br />കുട്ടി എൻജിനിയേഴ്സിന്  പറയാനുള്ളത് 
                 </P>
                 <Ul>
                     <Li>
                         <Name>
-
+                            Swathi Menon
                         </Name>
                         <School>
-
+                            8<Sup>Th</Sup>,Jamia Edavanna
                         </School>
-                        <Story>
+                        <Story className='story' >
                             <Play>
-                                <img src='' alt='Play' />
+                                <img src={playImage} alt='Play' />
                             </Play>
                             <Watch>
                                 WATCH STORY
@@ -35,15 +38,36 @@ export default function Tefun() {
         </TefunSection>
     )
 }
-const TefunSection = styled.section``
-const Wrapper = styled.section``
-const H3 = styled.h3``
-const B = styled.b``
-const P = styled.p``
+const TefunSection = styled.section`
+    padding: 5% 0;
+`
+const Wrapper = styled.section`
+    width: 90%;
+    margin: 0 auto;
+`
+const H3 = styled.h3`
+    font-weight: 900;
+    text-align: center;
+    font-size: 45px;
+    margin-bottom: 1%;
+`
+const B = styled.b`
+    color:#57a470;
+    font-weight:900;
+`
+const P = styled.p`
+    font-size: 23px;
+    text-align: center;
+    margin-bottom: 3%;
+`
 const Ul = styled.ul``
 const Li = styled.li``
 const Name = styled.h3``
 const School = styled.h4``
+const Sup = styled.b`
+    vertical-align: super;
+    font-size: small;
+`
 const Story = styled.button``
 const Play = styled.div``
 const Watch = styled.div``
