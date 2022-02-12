029aead5df67506b60fb215f1e5b5938a66dc680
swalihmoosa <swalihmoosa72@gmail.com>
2022-02-12 16:06:55 +0530

    Students | ongoing

--- src/components/screens/landing/Students.jsx
+++ src/components/screens/landing/Students.jsx
@@ -10,17 +10,18 @@ export default function Students() {
             <Wrapper>
                 <H4>
                     <B>
-
-                    </B>
+                        How
+                    </B><br />
+                    Students Can Join
                 </H4>
                 <Join>
                     <Left>
                         <Li>
                             <P>
-
+                            <Brand>Tefun</Brand> പ്രോഗ്രാം സ്കൂളിൽ ഉദ്ഘാടനം ചെയ്തതിനുശേഷം വിദ്യാർത്ഥികൾക്ക് <Brand>Tefun</Brand>-ൽ ജോയിൻ ചെയ്യാവുന്നതാണ്.
                             </P>
                             <Round>
-
+                                01
                             </Round>
                         </Li>
                     </Left>
@@ -33,12 +34,41 @@ export default function Students() {
     )
 }
 const StudentsSection = styled.section``
-const Wrapper = styled.section``
-const H4 = styled.section``
-const B = styled.section``
-const Join = styled.section``
-const Left = styled.section``
-const Li = styled.section``
-const P = styled.section``
-const Round = styled.section``
-const Right = styled.section``
+const Wrapper = styled.section`
+    width: 90%;
+    margin: 0 auto;
+`
+const H4 = styled.h4`
+    font-size: 35px;
+    font-weight: 900;
+    margin-bottom: 2%;
+`
+const B = styled.b`
+    font-size: 35px;
+    font-weight: 900;
+    color: #5aa870;
+`
+const Join = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 90%;
+    margin: 0 auto;
+`
+const Left = styled.ul`
+    width: 47%;
+`
+const Li = styled.li`
+    background: #ebf2fa;
+    padding: 5%;
+    border-radius: 25px;
+    &:first-child{
+        background: #fef6ec;
+    }
+`
+const P = styled.p``
+const Brand = styled.b``
+const Round = styled.p``
+const Right = styled.div`
+    width: 47%;
+`
