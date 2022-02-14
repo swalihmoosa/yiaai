6a4840380645e6452731a608c0c50441dc32dab2
swalihmoosa <swalihmoosa72@gmail.com>
2022-02-14 18:13:37 +0530

    VideoPage | ongoing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
+
 import Header from './components/includes/Header';
-import LandingPage from './components/screens/LandingPage';
+import VideoPage from './components/screens/VideoPage';
+// import LandingPage from './components/screens/LandingPage';
 
 
 function App() {
     return (
         <> 
             <Header />
-            <LandingPage />
+            <VideoPage />
         </>
     );
 }
-
 export default App;
---
+++ src/components/screens/VideoPage.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+export default function VideoPage() {
+    return (
+        <VideoPageSection>
+            <Wrapper>
+                <Video>
+                    <Thumbnail>
+
+                    </Thumbnail>
+                    <P>
+
+                    </P>
+                    <H4>
+
+                    </H4>
+                    <Steyp>
+                        <img src='' alt='Steyp' />
+                    </Steyp>
+                    <Tefun>
+                        <img src='' alt='Tefun' />
+                    </Tefun>
+                </Video>
+                <Ul>
+                    <Li>
+                        <Head>
+                            <Intro>
+
+                            </Intro>
+                            <Arrow>
+                                <img src='' alt='Arrow' />
+                            </Arrow>
+                        </Head>
+                        <SubHead>
+                            <PlayImage>
+                                <img src='' alt='Play' />
+                            </PlayImage>
+                            <Question>
+
+                            </Question>
+                            <Duration>
+                                
+                            </Duration>
+                        </SubHead>
+                    </Li>
+                </Ul>
+            </Wrapper>
+        </VideoPageSection>
+    )
+}
