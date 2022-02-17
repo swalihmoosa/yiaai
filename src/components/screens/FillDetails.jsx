e46dd4293532fd12fec0174d36e8935fccd918b8
swalihmoosa <swalihmoosa72@gmail.com>
2022-02-17 12:06:21 +0530

    FillDetails | ongoing

--- src/components/screens/FillDetails.jsx
+++ src/components/screens/FillDetails.jsx
@@ -34,10 +34,10 @@ export default function FillDetails() {
                         </Label>
                         <Divisions>
                             <Div>
-                                10
+                                A
                             </Div>
                             <Div>
-                                9
+                                B
                             </Div>
                         </Divisions>
                         <Buttons>
@@ -57,13 +57,14 @@ const Wrapper = styled.section`
     margin: 0 auto;
     width: 90%;
     background-color: #e4fdf7;
-    padding: 12%;
+    padding: 4%;
     border-radius: 50px;
 `
 const Details = styled.div`
     width: 50%;
     margin: 0 auto;
     border: 1px dotted #bfbfbf;
+    border-radius: 25px;
 `
 const H3 = styled.h3`
     padding: 5%;
@@ -99,13 +100,66 @@ const Classes = styled.select`
     padding: 4%;
     margin: 3% 0;
     border-radius: 15px;
+    background: url(http://cdn1.iconfinder.com/data/icons/cc_mono_icon_set/blacks/16x16/br_down.png) no-repeat right transparent;
+    -webkit-appearance: none;
+    background-position-x: 95%;
     &:focus{
         border: 2px solid #22c1c3;
     }
 `
-const Class = styled.option``
-const Divisions = styled.select``
-const Div = styled.option``
-const Buttons = styled.div``
-const Back = styled.button``
-const Submit = styled.button``
+const Class = styled.option`
+    font-size: 15px;
+    font-weight: 700;
+`
+const Divisions = styled.select`
+    width: 100%;
+    border: 2px solid #e3e3e3;
+    padding: 4%;
+    margin: 3% 0;
+    border-radius: 15px;
+    background: url(http://cdn1.iconfinder.com/data/icons/cc_mono_icon_set/blacks/16x16/br_down.png) no-repeat right transparent;
+    -webkit-appearance: none;
+    background-position-x: 95%;
+    &:focus{
+        border: 2px solid #22c1c3;
+    }
+`
+const Div = styled.option`
+    font-size: 15px;
+    font-weight: 700;
+`
+const Buttons = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-top: 7%;
+`
+const Back = styled.button`
+    width: 25%;
+    font-size: 18px;
+    font-weight: 900;
+    display: block;
+    color: #4f585e;
+    border: 2px solid #dfdfdf;
+    padding: 20px 30px;
+    border-radius: 10px;
+    margin-left: auto;
+    margin-right: 4%;
+    &:hover{
+        border: 2px solid #22c1c3;
+        cursor: pointer;
+    }
+`
+const Submit = styled.button`
+    width: 25%;
+    font-size: 18px;
+    font-weight: 900;
+    display: block;
+    color: #fff;
+    padding: 20px 30px;
+    border-radius: 10px;
+    background-image: linear-gradient(to right,#68ba50,#469aad);
+    &:hover{
+        background-image: linear-gradient(to right,#469aad,#68ba50);
+        cursor: pointer;
+    }
+`
