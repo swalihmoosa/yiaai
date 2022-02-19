4a0d8a8bf4a6674b76aa18ac9d27efa02c407132
swalihmoosa <swalihmoosa72@gmail.com>
2022-02-19 18:09:40 +0530

    ExamPage | ongoing

--- src/components/screens/ExamPage.jsx
+++ src/components/screens/ExamPage.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
+import leftImage from '../assets/images/tefun-certificate.svg'
+
 
 export default function ExamPage() {
     const [ selectedAnswer, setSelectedAnswer ] = useState("");
@@ -125,6 +127,25 @@ export default function ExamPage() {
                     </Next>
                 </ExamPaperContainer>
 
+                <WelcomeBg >
+                    <Welcome>
+                        <Left>
+                            <img src={leftImage} alt='Character' />
+                        </Left>
+                        <Right>
+                            <WelcomeHead>
+                                Congratulations
+                            </WelcomeHead>
+                            <Activated>
+                                You're Successfully Completed Your Examination, We will provide your certificate soon.
+                            </Activated>
+                            <Goto>
+                                Go to Dashboard
+                            </Goto>
+                        </Right>
+                    </Welcome>
+                </WelcomeBg>
+
             </Wrapper>
         </ExamPageSection>
     )
@@ -132,7 +153,9 @@ export default function ExamPage() {
 const ExamPageSection = styled.section`
     padding: 10% 0 2%;
 `
-const Wrapper = styled.section``
+const Wrapper = styled.section`
+    position: relative;
+`
 const Description = styled.div`
     width: 90%;
     background : #fdf3eb;
@@ -158,6 +181,7 @@ const Char = styled.div`
 `
 const Time = styled.div`
     margin-left: auto;
+    position: relative;
 
     &::before{
         position: absolute;
@@ -292,4 +316,69 @@ const NextNum = styled.p`
     font-weight: 700;
     left: -13px;
     top: 100%;
+`
+
+const WelcomeBg = styled.section`
+    position: absolute; 
+    left: 0; 
+    right: 0;
+    top: 50%;
+    margin : auto ;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    backdrop-filter: blur(10px);
+    border-radius: 50px;
+    transform: scale(1) translateY(-50%);
+    transition: all 0.6s ease 0s;
+    &.active{
+        transform: scale(1) translateY(-50%);
+    }
+`
+const Welcome= styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 55%;
+    margin: 0 auto;
+    align-items: center;
+    background: #e4fcf7;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    border-radius: 20px;
+    padding: 5%;
+`
+const Left = styled.div`
+    width: 45%;
+`
+const Right = styled.div`
+    width: 50%;
+`
+const WelcomeHead = styled.h3`
+    text-align: center;
+    font-size: 33px;
+    font-weight: 900;
+    color: #61b262;
+    margin-bottom: 5%;
+
+`
+const Activated = styled.p`
+    text-align: center;
+    font-size: 20px;
+    font-weight: 700;
+    color: #bfbfbf;
+    margin-bottom: 5%;
+`
+const Goto = styled.button`
+    width: 65%;
+    margin: 0 auto;
+    font-size: 18px;
+    font-weight: 900;
+    display: block;
+    color: #fff;
+    padding: 20px 30px;
+    border-radius: 10px;
+    background-image: linear-gradient(to right,#68ba50,#469aad);
+    &:hover{
+        cursor: pointer;
+    }
 `
