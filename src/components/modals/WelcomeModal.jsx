84485dfe0a9a537d53ddc74d74675f42b563b661
swalihmoosa <swalihmoosa72@gmail.com>
2022-03-05 17:06:10 +0530

    WelcomeModal | ongoing

---
+++ src/components/modals/WelcomeModal.jsx
@@ -0,0 +1,94 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+import leftImage from '../../assets/images/details/activated.svg'
+
+export default function WelcomeModal({isSubmitted}) {
+    return (
+        <WelcomeBg className={ isSubmitted ? 'active' : "" } >
+            <Welcome>
+                <Left>
+                    <img src={leftImage} alt='Character' />
+                </Left>
+                <Right>
+                    <WelcomeHead>
+                        Welcome
+                    </WelcomeHead>
+                    <Activated>
+                        You're Successfully activated Your Tefun Program
+                    </Activated>
+                    <Goto to="/video-page/" >
+                        Go to Dashboard
+                    </Goto>
+                </Right>
+            </Welcome>
+        </WelcomeBg>
+    )
+}
+const WelcomeBg = styled.section`
+    position: absolute; 
+    left: 0; 
+    right: 0;
+    top: 50%;
+    margin : auto ;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    backdrop-filter: blur(10px);
+    border-radius: 50px;
+    transform: scale(0) translateY(-50%);
+    transition: all 0.6s ease 0s;
+    z-index:1 ;
+    background-color:rgba(0,0,0,.2) ;
+    &.active{
+        transform: scale(1) translateY(-50%);
+    }
+`
+const Welcome= styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 55%;
+    margin: 0 auto;
+    align-items: center;
+    background: #e4fcf7;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    border-radius: 20px;
+`
+const Left = styled.div`
+    width: 45%;
+`
+const Right = styled.div`
+    width: 50%;
+`
+const WelcomeHead = styled.h3`
+    text-align: center;
+    font-size: 33px;
+    font-weight: 900;
+    color: #61b262;
+    margin-bottom: 5%;
+
+`
+const Activated = styled.p`
+    text-align: center;
+    font-size: 20px;
+    font-weight: 700;
+    color: #bfbfbf;
+    margin-bottom: 5%;
+`
+const Goto = styled(Link)`
+    width: 65%;
+    text-align: center;
+    margin: 0 auto;
+    font-size: 18px;
+    font-weight: 900;
+    display: block;
+    color: #fff;
+    padding: 20px 30px;
+    border-radius: 10px;
+    background-image: linear-gradient(to right,#68ba50,#469aad);
+    &:hover{
+        cursor: pointer;
+    }
+`
--- src/components/screens/FillDetails.jsx
+++ src/components/screens/FillDetails.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
 import bg from '../../assets/images/details/top.svg'
-import leftImage from '../../assets/images/details/activated.svg'
+import WelcomeModal from '../modals/WelcomeModal'
 
 
 export default function FillDetails() {
@@ -37,6 +37,8 @@ export default function FillDetails() {
     return (
         <FillDetailsSection>
             <Wrapper>
+                <WelcomeModal isSubmitted={isSubmitted} />
+
                 <Details>
                     <H3>
                         Fill Your Details
@@ -104,26 +106,7 @@ export default function FillDetails() {
                             <Submit onClick={()=> setIsSubmitted(true) }>Submit</Submit>
                         </Buttons>
                     </Form>
-                </Details>
-
-                <WelcomeBg className={ isSubmitted ? 'active' : "" } >
-                    <Welcome>
-                        <Left>
-                            <img src={leftImage} alt='Character' />
-                        </Left>
-                        <Right>
-                            <WelcomeHead>
-                                Welcome
-                            </WelcomeHead>
-                            <Activated>
-                                You're Successfully activated Your Tefun Program
-                            </Activated>
-                            <Goto to="/video-page/" >
-                                Go to Dashboard
-                            </Goto>
-                        </Right>
-                    </Welcome>
-                </WelcomeBg>
+                </Details>                
             </Wrapper>
         </FillDetailsSection>
     )
@@ -256,70 +239,6 @@ const Submit = styled.button`
     background-image: linear-gradient(to right,#68ba50,#469aad);
     &:hover{
         background-image: linear-gradient(to right,#469aad,#68ba50);
-        cursor: pointer;
-    }
-`
-const WelcomeBg = styled.section`
-    position: absolute; 
-    left: 0; 
-    right: 0;
-    top: 50%;
-    margin : auto ;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    backdrop-filter: blur(10px);
-    border-radius: 50px;
-    transform: scale(0) translateY(-50%);
-    transition: all 0.6s ease 0s;
-    &.active{
-        transform: scale(1) translateY(-50%);
-    }
-`
-const Welcome= styled.div`
-    display: flex;
-    justify-content: space-between;
-    width: 55%;
-    margin: 0 auto;
-    align-items: center;
-    background: #e4fcf7;
-    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-    border-radius: 20px;
-`
-const Left = styled.div`
-    width: 45%;
-`
-const Right = styled.div`
-    width: 50%;
-`
-const WelcomeHead = styled.h3`
-    text-align: center;
-    font-size: 33px;
-    font-weight: 900;
-    color: #61b262;
-    margin-bottom: 5%;
-
-`
-const Activated = styled.p`
-    text-align: center;
-    font-size: 20px;
-    font-weight: 700;
-    color: #bfbfbf;
-    margin-bottom: 5%;
-`
-const Goto = styled(Link)`
-    width: 65%;
-    text-align: center;
-    margin: 0 auto;
-    font-size: 18px;
-    font-weight: 900;
-    display: block;
-    color: #fff;
-    padding: 20px 30px;
-    border-radius: 10px;
-    background-image: linear-gradient(to right,#68ba50,#469aad);
-    &:hover{
         cursor: pointer;
     }
 `
